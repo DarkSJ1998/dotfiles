8fa80e966b0cd20b86c5516ed3e5f925098da39e
DarkSJ1998 <sarthakjn12@gmail.com>
2025-03-16 16:00:06 +0000

    Updated the script to add a function to backup custom zsh_configs

--- zsh-backup.sh
+++ zsh-backup.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+backup_zsh_configs() {
+    echo
+    echo "> Checking if the .zsh-configs directory exists in the user's directory..."
+    ZSH_CONFIGS_EXISTS=$(exec ls -a $HOME | grep -x .zsh-configs)
+    echo "ZSH_CONFIGS_EXISTS = \"$ZSH_CONFIGS_EXISTS\""
+
+    if [[ $ZSH_CONFIGS_EXISTS == ".zsh-configs" ]]
+    then
+        echo "> .zsh-configs directory exists"
+        echo 
+        read -p "Please confirm if you want to backup the contents under the .zsh-configs directory (Y/n): " BACKUP_CONSENT
+
+        # Changing the input to uppercase
+        BACKUP_CONSENT=${BACKUP_CONSENT^^}
+        # echo "BACKUP_CONSENT = \"$BACKUP_CONSENT\""
+
+        if [[ $BACKUP_CONSENT == "Y" ]]
+        then
+            echo "> Copying the \"$HOME/.zsh-configs\" directory to \"$CURRENT_DIR/zsh/.zsh-configs\" folder..."
+
+            ZSH_CONFIGS_EXISTS_REPO=$(exec dir $CURRENT_DIR/zsh | grep .zsh-configs)
+            echo ZSH_CONFIGS_EXISTS_REPO="$ZSH_CONFIGS_EXISTS_REPO"
+
+            if [[ -z $ZSH_CONFIGS_EXISTS_REPO ]]
+            then
+                echo "Folder doesn't exist, creating a folder..."
+                mkdir -p $CURRENT_DIR/zsh/.zsh-configs
+            fi
+
+            cp $HOME/.zsh-configs/* $CURRENT_DIR/zsh/.zsh-configs/
+            echo "> Done!"
+        else
+            echo "> Skipping this step"
+        fi
+
+    else
+        echo "> .zsh-configs doesn't exist, exiting"
+        exit
+    fi
+}
+
 # A bash script that copies the ZSH related files from the user's directory to the "zsh" directory inside this repo
 echo 
 echo "----------------------------------------"
@@ -47,3 +88,6 @@ then
 else
     echo "> Skipping this step"
 fi
+
+backup_zsh_configs
+
