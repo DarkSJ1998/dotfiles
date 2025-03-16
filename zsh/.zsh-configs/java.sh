34f4a4abe2c581e1d2c3b5f940552a52656830a3
DarkSJ1998 <sarthakjn12@gmail.com>
2025-03-16 16:00:38 +0000

    Added the configuration to use JDK 17

---
+++ zsh/.zsh-configs/java.sh
@@ -0,0 +1,8 @@
+# The configuration for JDK 17
+JAVA_HOME="/mnt/c/Program Files/Java/jdk-17"
+JAVA="$JAVA_HOME/bin"
+PATH="$PATH:$JAVA"
+
+# Creating an alias for ease of use
+alias java="java.exe"
+alias javac="javac.exe"
