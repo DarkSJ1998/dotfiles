b9e5e26cd745521175e2319535360a844cacbbc3
DarkSJ1998 <sarthakjn12@gmail.com>
2025-04-18 12:55:14 +0000

    Refactored java.sh to add Homebrew installation configuration

--- zsh/.zsh-configs/java.sh
+++ zsh/.zsh-configs/java.sh
@@ -1,8 +1,12 @@
-# The configuration for JDK 17
-JAVA_HOME="/mnt/c/Program Files/Java/jdk-17"
-JAVA="$JAVA_HOME/bin"
-PATH="$PATH:$JAVA"
+# The configuration for JDK 17 (using Windows installation)
+# JAVA_HOME="/mnt/c/Program Files/Java/jdk-17"
+# JAVA="$JAVA_HOME/bin"
+# alias java="java.exe"
+# alias javac="javac.exe"
 
-# Creating an alias for ease of use
-alias java="java.exe"
-alias javac="javac.exe"
+# The configuration for JDK (using Homebrew installation)
+export JAVA_HOME="/home/linuxbrew/.linuxbrew/Cellar/openjdk/23.0.2"
+export JAVA="$JAVA_HOME/bin"
+
+# Adding Java to the PATH
+export PATH="$PATH:$JAVA"
