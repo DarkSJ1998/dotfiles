00f79b5044ef4d9db133ab0d86e2974791579096
DarkSJ1998 <sarthakjn12@gmail.com>
2025-04-18 12:55:45 +0000

    Added Maven configuration script with Windows and Homebrew installation

---
+++ zsh/.zsh-configs/maven.sh
@@ -0,0 +1,11 @@
+# The configuration for Maven (using Windows installation)
+# export M2_HOME="/mnt/c/Program Files/apache-maven-3.9.4"
+# export M2="$M2_HOME/bin"
+# alias mvn="mvn.cmd"
+
+# The configuration for Maven (using Homebrew installation)
+export M2_HOME="/home/linuxbrew/.linuxbrew/Cellar/maven/3.9.9"
+export M2="$M2_HOME/bin"
+
+# Adding Maven to the PATH
+export PATH="$PATH:$M2"
