d9030f919cbd066f63613d1d2501143e79fab806
DarkSJ1998 <sarthakjn12@gmail.com>
2025-04-18 11:14:14 +0000

    Added the configuration for Homebrew

---
+++ zsh/.zsh-configs/homebrew.sh
@@ -0,0 +1,12 @@
+# ==> Next steps:
+# - Run these commands in your terminal to add Homebrew to your PATH:
+#     echo >> /home/darksj1998/.zshrc
+#     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/darksj1998/.zshrc
+#     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# - Install Homebrew's dependencies if you have sudo access:
+#     sudo apt-get install build-essential
+#   For more information, see:
+#     https://docs.brew.sh/Homebrew-on-Linux
+
+# The configuration for Homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
